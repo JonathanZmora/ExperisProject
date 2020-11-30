4b6802afba6640a2973dae89bba64a53ddcb9b68
User <jonathanzmora@gmail.com>
2020-11-30 15:13:06 +0200

    with main finished and working

--- cardGame/main_card_game.py
+++ cardGame/main_card_game.py
@@ -1,5 +1,3 @@
-from cardGame.Player import Player
-from cardGame.DeckOfCards import DeckOfCards
 from cardGame.CardGame import CardGame
 
 card_game = CardGame('yoav', 'yonatan')
@@ -11,31 +9,31 @@ for i in range(11):
     if card1.value > card2.value:
         card_game.player2.add_card(card1)
         card_game.player2.add_card(card2)
-        print(f'player 1 threw - {card1.__str__()}')
-        print(f'player 2 threw - {card2.__str__()}')
+        print(f'{card_game.player1.name} threw - {card1.__str__()}')
+        print(f'{card_game.player2.name} threw - {card2.__str__()}')
         print('the winner is ', card_game.player1.name)
 
     elif card2.value > card1.value:
         card_game.player1.add_card(card1)
         card_game.player1.add_card(card2)
-        print(f'player 1 threw - {card1.__str__()}')
-        print(f'player 2 threw - {card2.__str__()}')
+        print(f'{card_game.player1.name} threw - {card1.__str__()}')
+        print(f'{card_game.player2.name} threw - {card2.__str__()}')
         print('the winner is ', card_game.player2.name)
     else:
         if card1.suit > card2.suit:
             card_game.player2.add_card(card1)
             card_game.player2.add_card(card2)
-            print(f'player 1 threw - {card1.__str__()}')
-            print(f'player 2 threw - {card2.__str__()}')
+            print(f'{card_game.player1.name} threw - {card1.__str__()}')
+            print(f'{card_game.player2.name} threw - {card2.__str__()}')
             print('the winner is ', card_game.player1.name)
 
         else:
             card_game.player1.add_card(card1)
             card_game.player1.add_card(card2)
-            print(f'player 1 threw - {card1.__str__()}')
-            print(f'player 2 threw - {card2.__str__()}')
+            print(f'{card_game.player1.name} threw - {card1.__str__()}')
+            print(f'{card_game.player2.name} threw - {card2.__str__()}')
             print('the winner is ', card_game.player2.name)
 
-print('The winner is: ', card_game.get_winner())
+print('\nThe winner of the game is: ', card_game.get_winner())
 
 
