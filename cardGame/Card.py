c8d581a8d86bd763729331a8272eb4b1ce5e5f93
User <jonathanzmora@gmail.com>
2020-11-30 16:54:12 +0200

    with main finished and working, started notes

--- cardGame/Card.py
+++ cardGame/Card.py
@@ -1,11 +1,19 @@
-
+# a class that represents a single card
 class Card:
-
+    # constructor function, builds a card object
+    # gets the numeric value of the card and its suit
+    # and places them in attributes value and suit
     def __init__(self, value, suit):
         self.value = value
         self.suit = suit
 
+    # str function, returns card description:
+    # the value and the suit of the card
     def __str__(self):
+
+        # checks the suit number of the card
+        # to determine which ascii character to print
+        # 1 = diamond, 2 = spade, 3 = heart, 4 = club
         if self.suit == 1:
             return f'value: {self.value}, suit: ♦'
         elif self.suit == 2:
--- cardGame/DeckOfCards.py
+++ cardGame/DeckOfCards.py
@@ -2,24 +2,30 @@ from cardGame.Card import Card
 from random import *
 
 
+# a class that represents a full
+# deck of cards as a list
 class DeckOfCards:
 
+    # constructor function, builds a full deck of 52 cards
+    # creates a list named 'deck' and adds all the cards into it
     def __init__(self):
-        self.deck = []
-        for i in range(1, 14):
+        self.deck = []  # creation of list named 'deck'
+        for i in range(1, 14):  # loop for adding diamond suit
             self.deck += [Card(i, 1)]
 
-        for i in range(1, 14):
+        for i in range(1, 14):  # loop for adding spade suit
             self.deck += [Card(i, 2)]
 
-        for i in range(1, 14):
+        for i in range(1, 14):  # loop for adding heart suit
             self.deck += [Card(i, 3)]
 
-        for i in range(1, 14):
+        for i in range(1, 14):  # loop for adding club suit
             self.deck += [Card(i, 4)]
 
+    # shuffle function, shuffles the cards
+    # in the deck to a random order
     def shuffle(self):
-        shuffle(self.deck)
+        shuffle(self.deck)  # a python function, gets a list and shuffles the values
 
     def deal_one(self):
         index = randint(0, len(self.deck) - 1)
