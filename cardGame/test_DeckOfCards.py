2aa09cd84a9532a14f67a7fecac12fe957e4da69
yoavl <yoavlv122gmail.com>
2020-12-06 17:27:27 +0200

    deck_of_card_test test

---
+++ cardGame/test_DeckOfCards.py
@@ -0,0 +1,26 @@
+from unittest import TestCase
+from cardGame.CardGame import CardGame
+from cardGame.Player import Player
+from cardGame.Card import Card
+from random import *
+from cardGame.DeckOfCards import DeckOfCards
+
+
+class TestDeckOfCards(TestCase):
+    def setUp(self):
+        self.deck_test = DeckOfCards()
+
+    def test_shuffle(self):
+         list_of_cards = []
+         for i in self.deck_test.deck:
+                list_of_cards.append(i)
+         shuffle(list_of_cards)
+         self.assertListEqual(list_of_cards, self.deck_test.deck)
+
+    def test_deal_one(self):
+        list_1 = DeckOfCards()
+        list_2 = self.list_1.deal_one()
+        self.assertNotEquals(len(list_2), len(list_1))
+
+    def test_show(self):
+        self.fail()
