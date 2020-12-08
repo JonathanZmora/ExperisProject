d713fea9c7b954b89c8d6a1526fe2929f8146632
yoavl <yoavlv122gmail.com>
2020-12-08 10:02:31 +0200

    test describe

--- cardGame/test_Card.py
+++ cardGame/test_Card.py
@@ -3,27 +3,27 @@ from cardGame.Card import Card
 
 
 class TestCard(TestCase):
-
+    # If the init get corrrect values
     def test__init__working(self):
         card = Card(4, 2)
         self.assertTrue(card.value == 4 and card.suit == 2)
-
+    # if the value out of range (1-14) raise error
     def test__init__value_not_in_range(self):
         with self.assertRaises(ValueError):
             Card(15, 2)
-
+    # if the suit out of range (1-4) raise error
     def test__init__suit_not_in_range(self):
         with self.assertRaises(ValueError):
             Card(3, 5)
-
+    # if the value = init raise error
     def test__init__value_not_int(self):
         with self.assertRaises(ValueError):
             Card('d', 4)
-
+    # if the suit in the init != str raise error
     def test__init__suit_not_int(self):
         with self.assertRaises(ValueError):
             Card(3, 's')
-
+    # if the suit and the value in the init != str raise error
     def test__init__both_params_not_int(self):
         with self.assertRaises(ValueError):
             Card('s', 'y')
--- cardGame/test_CardGame.py
+++ cardGame/test_CardGame.py
@@ -8,38 +8,39 @@ class TestCardGame(TestCase):
     def setUp(self):
         self.game = CardGame('yonatan', 'yoav')
 
-    def test__init__working(self):
+    def test__init__working(self):     # check if the init work and get correct parameters
+
         game = CardGame('yonatan', 'yoav', 15)
         self.assertTrue(game.player1.name == 'yonatan' and game.player2.name)
         self.assertTrue(game.player1.num_of_cards == 15 and game.player2.num_of_cards == 15)
         self.assertTrue(len(game.player1.pack) == 15 and len(game.player2.pack) == 15)
         self.assertTrue(len(game.deck.deck) == 22)
 
-    def test__init__name1_is_not_string(self):
+    def test__init__name1_is_not_string(self):  # if name 1 !=  int raise error
         with self.assertRaises(ValueError):
             CardGame(765, 'yoav', 15)
 
-    def test__init__name2_is_not_string(self):
+    def test__init__name2_is_not_string(self):  # if name 2 !=  int raise error
         with self.assertRaises(ValueError):
             CardGame('yoav', 765, 15)
 
-    def test__init__name1_is_empty(self):
+    def test__init__name1_is_empty(self):   # if name 1 empty raise error
         with self.assertRaises(ValueError):
             CardGame('', 'yoav', 15)
 
-    def test__init__name2_is_empty(self):
+    def test__init__name2_is_empty(self):  # if name 2 empty raise error
         with self.assertRaises(ValueError):
             CardGame('yoav', '', 15)
 
-    def test__init__num_is_zero(self):
+    def test__init__num_is_zero(self):   # if number of cards = 0  raise error
         with self.assertRaises(ValueError):
             CardGame('yoav', 'yonatan', 0)
 
-    def test__init__num_is_negative(self):
+    def test__init__num_is_negative(self):   # if number of cards negative  raise error
         with self.assertRaises(ValueError):
             CardGame('yoav', 'yonatan', -1)
 
-    def test__init__num_not_int(self):
+    def test__init__num_not_int(self):  # if number of cards != int raise error
         with self.assertRaises(ValueError):
             CardGame('yoav', 'yonatan', '1')
 
@@ -52,15 +53,15 @@ class TestCardGame(TestCase):
         self.game.new_game()
         self.assertEqual(len(self.game.player1.pack), 1)
 
-    def test_get_winner_player_1(self):
+    def test_get_winner_player_1(self):   # test while the pack of player 1 smaller , player 1 winner
         self.game.player1.pack.pop()
         self.assertEqual(self.game.get_winner(), 'yonatan')
 
-    def test_get_winner_player_2(self):
+    def test_get_winner_player_2(self):  # test while the pack of player 2 smaller , player 2 winner
         self.game.player2.pack.pop()
         self.assertEqual(self.game.get_winner(), 'yoav')
 
-    def test_get_winner_draw(self):
+    def test_get_winner_draw(self):  # test while the pack of player 1 equel to player 2 winner get "None"
         self.assertEqual(self.game.get_winner(), 'None')
 
     def test_play_round_player1_value_greater(self):
